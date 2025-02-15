bafdbfe05877e6755ae5d066bb1c30b6fda8ae98
shaguftanaliyawala <shaguftasalman24@gmail.com>
2025-02-16 02:39:48 +0500

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "http://mindmechanicss.com", // Change to your production domain later
+  generateRobotsTxt: true, // Generates robots.txt as well
+};  
