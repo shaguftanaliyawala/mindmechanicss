a2d283add5749820e0c8e55236a46e1eb6f849f8
muhamadsami23 <155666362+muhamadsami23@users.noreply.github.com>
2026-08-15 16:56:54 +0500

    Updated social links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -74,7 +74,7 @@ export function Footer() {
                 </a>
               </li>
               <li>
-                <a href="https://www.instagram.com/mindmechanics09" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-primary">
+                <a href="https://www.instagram.com/mind_mechanics09" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-primary">
                   <Instagram className="w-4 h-4" /> Instagram
                 </a>
               </li>
